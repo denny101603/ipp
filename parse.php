fb8deabb67b537a3b007a6138942dc4721934db5
denny101603 <43618398+denny101603@users.noreply.github.com>
2019-02-12 20:01:47 +0100

    soubor

--- parse.php
+++ parse.php
@@ -1,3 +1,16 @@
 <?php
-echo "hello";
+$line = readline();
+if(strcmp($line, ".IPPcode19"))
+{
+    fprintf(STDERR, "Spatna hlavicka ve zdrojovem kodu");
+    exit(21);
+}
+$line = readline();
+$patternVar = "^[LTG]F@[\_\-$&%*!?a-zA-Z][\_\-$&%*!?a-zA-Z0-9]*";
+if(preg_match("/^MOVE/", $line))
+{
+    echo "je to MOVE";
+}
+echo $line."bla";
+exit(0);
 ?>
