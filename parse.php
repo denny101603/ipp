f4db1fee9f80027226e91054ade9c4befe59be48
denny101603 <43618398+denny101603@users.noreply.github.com>
2019-02-18 12:56:50 +0100

    pokusy s php

--- parse.php
+++ parse.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * @param $line
+ * @param $xml
+ */
+function checkVar($line, $xml)
+{
+    $patternVar = "^[LTG]F@([\_\-$&%*!?a-zA-Z][\_\-$&%*!?a-zA-Z0-9]*)^";
+    if(preg_match($patternVar, $line, $matches))
+    {
+        print("nalezeno var: ".$matches[1]."\n");
+        $xml->addChild('arg', );
+    }
+    else
+        print("var se nenasel\n");
+}
+
 $line = readline();
 if(strcmp($line, ".IPPcode19"))
 {
@@ -6,11 +22,17 @@ if(strcmp($line, ".IPPcode19"))
     exit(21);
 }
 $line = readline();
-$patternVar = "^[LTG]F@[\_\-$&%*!?a-zA-Z][\_\-$&%*!?a-zA-Z0-9]*";
+$xmlOut = new SimpleXMLElement('<xml/>');
+checkVar($line, $xmlOut);
+
 if(preg_match("/^MOVE/", $line))
 {
     echo "je to MOVE";
 }
 echo $line."bla";
+
+$instr = $xmlOut->addChild('instr', 'MOVE');
+$instr->addChild('arg', 'nejaky argument');
+print($xmlOut->asXML());
 exit(0);
 ?>
