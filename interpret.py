ee85b57f825325ef3b570579f8629901b7d70902
Daniel Bubeníček <xbuben05@vutbr.cz>
2019-03-30 13:47:55 +0100

    zacatek interpretu

---
+++ interpret.py
@@ -0,0 +1,13 @@
+print("start")
+
+class Instruction:
+    def __init__(self, opCode, args):
+        opCodeType =opCode
+        self.args = args
+
+
+
+class Argument:
+    def __init__(self, type, value):
+        self.type = type
+
